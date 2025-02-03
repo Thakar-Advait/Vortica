aaa56df54bc25db095ae1b6a18d24480c451bcf5
Advait Thakar <advaitthakar@Advaits-MacBook-Air.local>
2025-02-03 16:13:13 +0530

    resolved

--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import { DB_NAME } from "../../constants.js";aw
 
 const connectDb = async ()=>{
     try {
-        const dbUri = process.env.DB_URI || 'mongodb+srv://advait-thakar:thakaradvait1234@cluster0.btxsahr.mongodb.net';
+        const dbUri = process.env.DB_URI;
         const connectionInstance = await mongoose.connect(`${dbUri}/${DB_NAME}`)
 
         console.log(`\nMongoDB connected! DB_HOST: ${connectionInstance.connection.host}`)
